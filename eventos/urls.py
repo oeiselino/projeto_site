e87b076ef7fb07187d9d8c598bedebfffcc858ff
oeiselino <oeiselino@hotmail.com>
2013-07-16 08:34:10 -0300

    Delete urls.py

--- eventos/urls.py
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-
-from django.conf.urls import patterns, include, url
-from views import EventoListaView, EventoDetalheView
-
-urlpatterns = patterns('eventos.views',
-    url(r'^$', EventoListaView.as_view(), name='eventos'),
-    url(r'^(?P<pk>\d+)/$', EventoDetalheView.as_view(), name='eventos_detalhes'),
-)
